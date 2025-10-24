00d472575eb21493ffe92ec2ccb8710809880a79
ibiamzy70 <victoribiamu@gmail.com>
2025-10-24 13:55:11 +0100

    django setup

---
+++ accounts/serializers.py
@@ -0,0 +1,47 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+
+User = get_user_model()
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        validators=[validate_password],
+        style={"input_type": "password"}
+    )
+    password2 = serializers.CharField(
+        write_only=True,
+        required=True,
+        style={"input_type": "password"}
+    )
+
+    class Meta:
+        model = User
+        fields = ["email", "full_name", "role", "password", "password2"]
+
+    def validate_email(self, value):
+        
+        if User.objects.filter(email__iexact=value).exists():
+            raise serializers.ValidationError("A user with this email already exists.")
+        return value
+
+    def validate(self, attrs):
+        
+        if attrs.get("password") != attrs.get("password2"):
+            raise serializers.ValidationError({"password": "Passwords do not match."})
+        return attrs
+
+    def create(self, validated_data):
+        
+        validated_data.pop("password2")
+        password = validated_data.pop("password")
+
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+
+        return user
